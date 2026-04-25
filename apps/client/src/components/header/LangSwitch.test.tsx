125d278d01ab808bf22c9b31ee270f42bebdc706
sergey-ado <adonisiy2@gmail.com>
2026-04-25 20:52:24 +0400

    feat: add tests for LangSwitch

---
+++ apps/client/src/components/header/LangSwitch.test.tsx
@@ -0,0 +1,59 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { LangSwitch } from './LangSwitch';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+
+beforeEach(async () => {
+  await i18next.init({
+    lng: 'en',
+    supportedLngs: ['en', 'ru'],
+    fallbackLng: 'en',
+    resources: {
+      en: { translation: {} },
+      ru: { translation: {} },
+    },
+  });
+});
+
+function renderWithProvider(ui: React.ReactElement) {
+  return render(<I18nextProvider i18n={i18next}>{ui}</I18nextProvider>);
+}
+
+describe('LangSwitch', () => {
+  it('sets the default language to en', () => {
+    renderWithProvider(<LangSwitch />);
+
+    expect(screen.getByText('en')).toBeInTheDocument();
+    expect(screen.getByText('ru')).toBeInTheDocument();
+    expect(i18next.language).toBe('en');
+  });
+
+  it('changes the language when you click on the switch', async () => {
+    renderWithProvider(<LangSwitch />);
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText('ru'));
+    expect(i18next.language).toBe('ru');
+
+    await user.click(screen.getByText('en'));
+    expect(i18next.language).toBe('en');
+  });
+
+  it('calls the catch function if changeLanguage fails', async () => {
+    const changeLanguageMock = vi
+      .spyOn(i18next, 'changeLanguage')
+      .mockRejectedValue(new Error('Test error'));
+
+    render(<LangSwitch />);
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText('en'));
+
+    expect(changeLanguageMock).toHaveBeenCalled();
+
+    changeLanguageMock.mockRestore();
+  });
+});
--- apps/client/src/components/header/LangSwitch.tsx
+++ apps/client/src/components/header/LangSwitch.tsx
@@ -1,21 +1,14 @@
 import i18next from 'i18next';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 export function LangSwitch() {
   const [lang, setLang] = useState(i18next.language);
-
-  const { i18n } = useTranslation();
 
   const handleLangChange = () => {
     const newLang = lang === 'en' ? 'ru' : 'en';
     i18next.changeLanguage(newLang).catch(() => {});
     setLang(newLang);
   };
-
-  useEffect(() => {
-    document.body.dir = i18n.dir();
-  }, [i18n, i18n.language]);
 
   return (
     <div className="bg-blue-600 w-20 h-11 rounded-full flex items-center text-xl text-white relative border-2">
@@ -26,6 +19,7 @@ export function LangSwitch() {
         ru
       </div>
       <div
+        role="presentation"
         className={`bg-white w-8 h-8 rounded-full absolute left-${lang === 'en' ? 1 : 10} cursor-auto duration-200`}></div>
     </div>
   );
