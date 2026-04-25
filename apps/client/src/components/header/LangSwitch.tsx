06e798510914f8b6aba12ead4a6320f155de6bfd
sergey-ado <adonisiy2@gmail.com>
2026-04-25 16:28:23 +0400

    feat: add i18next

--- apps/client/src/components/header/LangSwitch.tsx
+++ apps/client/src/components/header/LangSwitch.tsx
@@ -1,20 +1,32 @@
-import { useState } from 'react';
+import i18next from 'i18next';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export function LangSwitch() {
-  const [isLeft, setIsLeft] = useState(true);
+  const [lang, setLang] = useState(i18next.language);
 
-  const onClick = () => setIsLeft(!isLeft);
+  const { i18n } = useTranslation();
+
+  const handleLangChange = () => {
+    const newLang = lang === 'en' ? 'ru' : 'en';
+    i18next.changeLanguage(newLang).catch(() => {});
+    setLang(newLang);
+  };
+
+  useEffect(() => {
+    document.body.dir = i18n.dir();
+  }, [i18n, i18n.language]);
 
   return (
     <div className="bg-blue-600 w-20 h-11 rounded-full flex items-center text-xl text-white relative border-2">
-      <div className="ml-2.5 cursor-pointer" onClick={onClick}>
+      <div className="ml-2.5 cursor-pointer" onClick={handleLangChange}>
         en
       </div>
-      <div className="ml-2.5 cursor-pointer" onClick={onClick}>
+      <div className="ml-2.5 cursor-pointer" onClick={handleLangChange}>
         ru
       </div>
       <div
-        className={`bg-white w-8 h-8 rounded-full absolute left-${isLeft ? 1 : 10} cursor-auto duration-200`}></div>
+        className={`bg-white w-8 h-8 rounded-full absolute left-${lang === 'en' ? 1 : 10} cursor-auto duration-200`}></div>
     </div>
   );
 }
--- apps/client/src/components/pages/welcome/Welcome.tsx
+++ apps/client/src/components/pages/welcome/Welcome.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 function onLoginClick() {
   console.log('login');
 }
@@ -7,32 +9,37 @@ function onRegisterClick() {
 }
 
 export function Welcome() {
+  const { t } = useTranslation();
+
+  const welcome = t('welcome.welcome');
+  const inGame = t('welcome.in-game');
+  const desc = t('welcome.desc');
+  const login = t('welcome.login');
+  const register = t('welcome.register');
+
   return (
     <main className="grow flex justify-center items-center">
       <div className="flex flex-col gap-4 items-center max-w-160 border rounded-lg bg-[#27272773] shadow-[0_0_20px_0_#27272773] py-8 px-8 text-white mx-8 border-white">
-        <h2 className="max-[450px]:text-2xl text-3xl text-shadow-[3px_3px_rgb(0,0,0)]">
-          Добро пожаловать
+        <h2 className="max-[450px]:text-2xl text-3xl text-shadow-[3px_3px_rgb(0,0,0)] capitalize">
+          {welcome}
         </h2>
-        <span>в игру</span>
+        <span>{inGame}</span>
         <h1 className="uppercase max-[450px]:text-4xl text-5xl font-perm text-amber-300 max-[450px]:my-1 my-4 text-shadow-[3px_3px_rgb(0,0,0)]">
           codenames
         </h1>
-        <p className="text-center text-shadow-[3px_3px_black]">
-          Здесь вы, играя, сможете улучшить и проверить свои знания в таких
-          популярных языках как JavaScript и TypeScript
-        </p>
+        <p className="text-center text-shadow-[3px_3px_black]">{desc}</p>
         <div className="flex gap-4 mt-6 text-black">
           <button
             type="button"
-            className="py-2 px-4 bg-white rounded-lg capitalize hover:bg-[rgb(147,147,147)] duration-300 outline-none w-25 cursor-pointer"
+            className="py-2 px-4 bg-white rounded-lg capitalize hover:bg-[rgb(147,147,147)] duration-300 outline-none cursor-pointer"
             onClick={onLoginClick}>
-            login
+            {login}
           </button>
           <button
             type="button"
-            className="py-2 px-4 bg-white rounded-lg capitalize hover:bg-[rgb(147,147,147)] duration-300 outline-none w-25 cursor-pointer"
+            className="py-2 px-4 bg-white rounded-lg capitalize hover:bg-[rgb(147,147,147)] duration-300 outline-none cursor-pointer"
             onClick={onRegisterClick}>
-            register
+            {register}
           </button>
         </div>
       </div>
