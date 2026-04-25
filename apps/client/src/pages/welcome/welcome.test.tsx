5004990049eea56fc2cce2f3fcb81695b6aa2092
sergey-ado <adonisiy2@gmail.com>
2026-04-25 10:17:21 +0400

    feat: add tests for welcome page

---
+++ apps/client/src/pages/welcome/welcome.test.tsx
@@ -0,0 +1,39 @@
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Welcome } from './Welcome';
+
+describe('Welcome', () => {
+  it('render headers', () => {
+    render(<Welcome />);
+    expect(screen.getByText(/^codenames$/i)).toBeInTheDocument();
+  });
+
+  it('calls onLoginClick when you click on LOGIN', async () => {
+    const user = userEvent.setup();
+
+    const spy = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+    render(<Welcome />);
+    const loginButton = screen.getByRole('button', { name: /login/i });
+
+    await user.click(loginButton);
+    expect(spy).toHaveBeenCalledWith('login');
+
+    spy.mockRestore();
+  });
+
+  it('calls onRegisterClick when you click on REGISTER', async () => {
+    const user = userEvent.setup();
+
+    const spy = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+    render(<Welcome />);
+    const registerButton = screen.getByRole('button', { name: /register/i });
+
+    await user.click(registerButton);
+    expect(spy).toHaveBeenLastCalledWith('register');
+
+    spy.mockRestore();
+  });
+});
