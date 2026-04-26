0846013e5ecbe65678aa3a2c6042703808f9f4d8
sergey-ado <adonisiy2@gmail.com>
2026-04-26 21:46:36 +0400

    feat: move general class for buttons and containers to sepatete classes

--- apps/client/src/app/components/themeSwitch/ThemeSwitch.tsx
+++ apps/client/src/app/components/themeSwitch/ThemeSwitch.tsx
@@ -10,13 +10,15 @@ export function ThemeSwitch() {
 
   useEffect(() => {
     const root = document.documentElement;
+    root.classList.remove('can-dur');
     if (theme === 'dark') root.classList.add('dark');
     else root.classList.remove('dark');
     localStorage.setItem('theme', theme);
+    root.classList.add('change-theme');
   }, [theme]);
 
   return (
-    <div className="bg-blue-200 dark:bg-amber-950 w-20 h-11 rounded-full flex items-center text-xl dark:text-white relative border">
+    <div className="switch-base">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -47,8 +49,7 @@ export function ThemeSwitch() {
           d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
         />
       </svg>
-      <div
-        className={`bg-white dark:bg-amber-700 w-8 h-8 rounded-full absolute left-${theme === 'light' ? 1 : 10} cursor-auto duration-200 border`}></div>
+      <div className={`switch-circle left-${theme === 'light' ? 1 : 10}`}></div>
     </div>
   );
 }
--- apps/client/src/app/pages/welcome/Welcome.tsx
+++ apps/client/src/app/pages/welcome/Welcome.tsx
@@ -19,7 +19,7 @@ export function Welcome() {
 
   return (
     <main className="grow flex justify-center items-center">
-      <div className="flex flex-col gap-4 items-center max-w-160 border rounded-lg bg-[#ffffff73] dark:bg-[#00000073] shadow-[0_0_20px_0_#27272773] py-8 px-8 mx-8 border-black dark:border-white max-[360px]:px-8 max-[360px]:mx-4">
+      <div className="container flex flex-col gap-4 items-center max-w-160  py-8 px-8 mx-8 max-[360px]:px-8 max-[360px]:mx-4">
         <h2 className="max-[450px]:text-2xl text-3xl capitalize">{welcome}</h2>
         <span>{inGame}</span>
         <h1 className="uppercase max-[450px]:text-4xl text-5xl font-perm text-blue-600 dark:text-amber-300 max-[450px]:my-1 my-4 text-shadow-[4px_4px_rgb(0,0,0)]">
@@ -29,13 +29,13 @@ export function Welcome() {
         <div className="flex gap-4 mt-6 text-black">
           <button
             type="button"
-            className="py-2 px-4 bg-white dark:bg-amber-700 rounded-lg capitalize hover:bg-blue-200 dark:hover:bg-amber-500 duration-300 outline-none cursor-pointer border dark:text-white"
+            className="button py-2 px-4"
             onClick={onLoginClick}>
             {login}
           </button>
           <button
             type="button"
-            className="py-2 px-4 bg-white dark:bg-amber-700 rounded-lg capitalize hover:bg-blue-200 dark:hover:bg-amber-500 duration-300 outline-none cursor-pointer border dark:text-white"
+            className="button py-2 px-4"
             onClick={onRegisterClick}>
             {register}
           </button>
