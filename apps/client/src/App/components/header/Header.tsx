4829445a33b7dc60cefbd33fb328c5d520d76b2f
sergey-ado <adonisiy2@gmail.com>
2026-04-26 08:45:52 +0400

    feat: add ThemeSwitch

--- apps/client/src/App/components/header/Header.tsx
+++ apps/client/src/App/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import { JSX } from 'react';
-import { LangSwitch } from '../LangSwitch/LangSwitch';
+import { LangSwitch } from '../langSwitch/LangSwitch';
+import { ThemeSwitch } from '../themeSwitch/ThemeSwitch';
 
 export function Header(): JSX.Element {
   return (
-    <header className="w-screen max-w-7xl p-5">
+    <header className="w-screen max-w-7xl p-5 flex gap-5">
       <LangSwitch />
+      <ThemeSwitch />
     </header>
   );
 }
