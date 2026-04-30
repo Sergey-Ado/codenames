405a4afad226a14c252e15c06aa7c306f0cbbeb4
sergey-ado <adonisiy2@gmail.com>
2026-04-30 06:57:27 +0400

    feat: add logo

--- apps/client/src/app/components/header/Header.tsx
+++ apps/client/src/app/components/header/Header.tsx
@@ -1,10 +1,13 @@
 import { JSX } from 'react';
 import { LangSwitch } from '../langSwitch/LangSwitch';
 import { ThemeSwitch } from '../themeSwitch/ThemeSwitch';
+import { Logo } from './logo/logo';
 
 export function Header(): JSX.Element {
   return (
-    <header className="w-screen max-w-7xl p-5 flex gap-5">
+    <header className="w-screen max-w-7xl p-5 flex gap-5 items-center">
+      <Logo />
+      <div className="grow"></div>
       <LangSwitch />
       <ThemeSwitch />
     </header>
---
+++ apps/client/src/app/components/header/logo/logo.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const onClick = () => {
+  console.log('logo');
+};
+
+export function Logo() {
+  const [text, setText] = useState('codenames');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 460) setText('cn');
+      else setText('codenames');
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <>
+      <span
+        className="uppercase text-3xl font-perm text-third-light dark:text-amber-300 max-[450px]:my-1 text-shadow-[4px_4px_4px_rgb(0,0,0)] cursor-pointer"
+        onClick={onClick}>
+        {text}
+      </span>
+    </>
+  );
+}
