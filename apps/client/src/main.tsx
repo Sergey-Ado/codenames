0aa9e390695844fc84061e2afaa79272fec3870f
sergey-ado <adonisiy2@gmail.com>
2026-04-30 17:15:36 +0400

    feat: add router

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App.tsx';
 import './i18n.tsx';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import { LoginPage } from './app/pages/loginPage/LoginPage.tsx';
+import { WelcomePage } from './app/pages/welcomePage/WelcomePage.tsx';
+import { RegisterPage } from './app/pages/registerPage/RegisterPage.tsx';
+import { ErrorPage } from './app/pages/errorPage/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+    children: [
+      { index: true, Component: WelcomePage },
+      { path: 'login', Component: LoginPage },
+      { path: 'register', Component: RegisterPage },
+      {
+        path: '*',
+        Component: ErrorPage,
+      },
+    ],
+  },
+]);
 
 createRoot(document.querySelector('#root') || document.body).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
 
 document.documentElement.classList.add('can-dur');
