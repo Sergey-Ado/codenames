98594c0660437001b45f7154c80e55a5e5a4e577
sergey-ado <adonisiy2@gmail.com>
2026-05-05 21:05:19 +0400

    feat: add tests for base of Lobby page

---
+++ apps/client/src/app/pages/lobbyPage/LobbyPage.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { LobbyPage } from './LobbyPage';
+
+describe('LobbyPage', () => {
+  it('render base', () => {
+    render(<LobbyPage />);
+    expect(screen.getByText(/Login/i)).toBeInTheDocument();
+  });
+});
