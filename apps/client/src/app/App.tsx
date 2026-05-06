97f2d59566418c3eef6fc37dd74fcb05095dd35a
sergey-ado <adonisiy2@gmail.com>
2026-05-06 18:50:11 +0400

    feat: add SettingsModal

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from 'sonner';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-[url(/images/background-light.jpg)] dark:bg-[url(/images/background-dark.jpg)] bg-no-repeat bg-fixed bg-center bg-cover font-rub select-none text-text-light dark:text-text-dark">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-[url(/images/background-light.jpg)] dark:bg-[url(/images/background-dark.jpg)] bg-no-repeat bg-fixed bg-center bg-cover font-rub select-none text-text-light dark:text-text-dark relative">
       <Header />
       <Outlet />
       <Toaster position="top-center" richColors duration={2000} />
--- apps/client/src/app/components/header/Header.tsx
+++ apps/client/src/app/components/header/Header.tsx
@@ -1,9 +1,8 @@
-import { JSX } from 'react';
 import { LangSwitch } from '../langSwitch/LangSwitch';
 import { ThemeSwitch } from '../themeSwitch/ThemeSwitch';
 import { Logo } from './logo/Logo';
 
-export function Header(): JSX.Element {
+export function Header() {
   return (
     <header className="w-screen max-w-7xl p-5 flex gap-5 items-center">
       <Logo />
