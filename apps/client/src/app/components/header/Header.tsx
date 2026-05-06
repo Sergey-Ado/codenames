ebf49e1b4bfcf7676b76579c3e3a4ea577fc84f8
sergey-ado <adonisiy2@gmail.com>
2026-05-06 22:40:47 +0400

    feat: remove themeSwitcher and langSwitcher from header

--- apps/client/src/app/components/header/Header.tsx
+++ apps/client/src/app/components/header/Header.tsx
@@ -1,5 +1,3 @@
-import { LangSwitch } from '../langSwitch/LangSwitch';
-import { ThemeSwitch } from '../themeSwitch/ThemeSwitch';
 import { Logo } from './logo/Logo';
 
 export function Header() {
@@ -7,8 +5,6 @@ export function Header() {
     <header className="w-screen max-w-7xl p-5 flex gap-5 items-center">
       <Logo />
       <div className="grow"></div>
-      <LangSwitch />
-      <ThemeSwitch />
     </header>
   );
 }
