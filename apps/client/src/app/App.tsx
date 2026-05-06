874474522188109c563739e18715e3a2dab227c4
sergey-ado <adonisiy2@gmail.com>
2026-05-06 18:59:40 +0400

    feat: add SettingsButton

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 import { Header } from './components/header/Header';
 import { Toaster } from 'sonner';
+import { SettingsButton } from './components/settingsButton/SettingsButton';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Outlet />
       <Toaster position="top-center" richColors duration={2000} />
+      <SettingsButton />
     </div>
   );
 }
