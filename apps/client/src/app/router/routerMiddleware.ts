aeab6f1b9d0bbf3481a1535a6d058b965a98a633
sergey-ado <adonisiy2@gmail.com>
2026-05-11 10:05:04 +0400

    feat: add promise to routerMiddleware

--- apps/client/src/app/router/routerMiddleware.ts
+++ apps/client/src/app/router/routerMiddleware.ts
@@ -2,7 +2,7 @@ import { Pages, StorageConstants } from '@/types/general.types';
 import { MiddlewareFunction, redirect } from 'react-router';
 import { socket } from './router';
 
-export const routerMiddleware: MiddlewareFunction = ({ request }) => {
+export const routerMiddleware: MiddlewareFunction = async ({ request }) => {
   const protectedPages = new Set([Pages.LOBBY].map(String));
 
   const authToken = sessionStorage.getItem(StorageConstants.AUTH_TOKEN);
@@ -13,17 +13,29 @@ export const routerMiddleware: MiddlewareFunction = ({ request }) => {
       throw redirect(`/${Pages.LOBBY}`);
     }
 
-    socket.on('connect_error', () => {
+    await new Promise((res, rej) => {
+      socket.auth = { authToken };
+      socket.disconnect().connect();
+
+      const connectListener = () => {
+        socket.off('connect', connectListener);
+        res('Ok');
+      };
+
+      const connectErrorListener = () => {
+        socket.off('connect_error', connectErrorListener);
+        rej();
+      };
+
+      socket.on('connect', connectListener);
+      socket.on('connect_error', connectErrorListener);
+    }).then(console.log, () => {
       sessionStorage.removeItem(StorageConstants.AUTH_TOKEN);
       sessionStorage.removeItem(StorageConstants.USER_ID);
       sessionStorage.removeItem(StorageConstants.USERNAME);
+
       throw redirect(`/${Pages.LOBBY}`);
     });
-
-    if (!socket.connected) {
-      socket.auth = { authToken };
-      socket.connect();
-    }
   } else {
     if (protectedPages.has(urlEnd)) {
       throw redirect(`/${Pages.LOGIN}`);
