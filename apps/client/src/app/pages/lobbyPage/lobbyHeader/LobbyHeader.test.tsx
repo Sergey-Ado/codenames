18bf5b3278e3e6398bdda8433fff309c20a28ef2
sergey-ado <adonisiy2@gmail.com>
2026-06-07 16:37:46 +0400

    feat: update tests for LobbyHeader

--- apps/client/src/app/pages/lobbyPage/lobbyHeader/LobbyHeader.test.tsx
+++ apps/client/src/app/pages/lobbyPage/lobbyHeader/LobbyHeader.test.tsx
@@ -1,19 +1,38 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { LobbyHeader } from './LobbyHeader';
+import userEvent from '@testing-library/user-event';
 
 describe('LobbyHeader', () => {
-  it('should render', () => {
+  it('should be rendered', () => {
     render(<LobbyHeader />);
 
     expect(screen.queryByRole('button')).toBeInTheDocument();
   });
 
-  it('should call console.log when click button', () => {
+  it('should render room create form when click button', () => {
     render(<LobbyHeader />);
 
     fireEvent.click(screen.getByRole('button'));
 
     expect(screen.queryByRole('room-create-form')).toBeInTheDocument();
+  });
+
+  it('should close room create form when submit new room data', async () => {
+    render(<LobbyHeader />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(screen.queryByRole('room-create-form')).toBeInTheDocument();
+
+    const user = userEvent.setup();
+
+    const inputName = screen.getByRole('input-name');
+    await user.type(inputName, 'new-room');
+
+    const submit = screen.getByRole('submit');
+    await user.click(submit);
+
+    expect(screen.queryByRole('room-create-form')).not.toBeInTheDocument();
   });
 });
