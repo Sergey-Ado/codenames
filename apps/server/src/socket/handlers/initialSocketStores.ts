4b859abaf99a5f542829bd98e0158c6a80750de3
sergey-ado <adonisiy2@gmail.com>
2026-06-19 06:06:18 +0400

    feat: add SocketStores singleton

--- apps/server/src/socket/handlers/initialSocketStores.ts
+++ apps/server/src/socket/handlers/initialSocketStores.ts
@@ -2,13 +2,25 @@ import { SocketIdsMap } from '../../types/types.ts';
 import { mockRooms } from '../data/mockRooms.ts';
 import { RoomManager } from '../roomManager/roomManager.ts';
 
+class SocketStores {
+  private static instance: SocketStores | undefined;
+  public socketIdsMap: SocketIdsMap = new Map();
+  public roomManager: RoomManager = new RoomManager();
+
+  public constructor() {
+    if (SocketStores.instance) return SocketStores.instance;
+
+    this.roomManager.setRooms(mockRooms);
+
+    SocketStores.instance = this;
+  }
+}
+
 export const initialSocketStores = (): {
   socketIdsMap: SocketIdsMap;
   roomManager: RoomManager;
 } => {
-  const socketIdsMap: SocketIdsMap = new Map();
-  const roomManager = new RoomManager();
-  roomManager.setRooms(mockRooms);
+  const { socketIdsMap, roomManager } = new SocketStores();
 
   return { socketIdsMap, roomManager };
 };
