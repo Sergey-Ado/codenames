a42b4bed94675f5af5b45bca15ea076cbb9d6bd5
sergey-ado <adonisiy2@gmail.com>
2026-06-20 05:26:30 +0400

    feat: add search input and button

---
+++ apps/client/src/app/pages/lobbyPage/lobbyHeader/lobbySearch/LobbySearch.tsx
@@ -0,0 +1,58 @@
+import { ChangeEvent, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export function LobbySearch() {
+  const [value, setValue] = useState('');
+
+  const onCrossClick = () => {
+    setValue('');
+    console.log('clear');
+  };
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setValue(value);
+  };
+
+  const onSearchClick = () => {
+    if (value) {
+      console.log(`search ${value}`);
+    }
+  };
+
+  const { t } = useTranslation();
+
+  const search = t('lobby.search');
+
+  const cross = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="-ml-11 h-full pl-1 pr-1 ph-0 w-8 cursor-pointer"
+      onClick={onCrossClick}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+
+  return (
+    <div className="flex items-center gap-2.5 justify-end grow sm:gap-3">
+      <input
+        type="text"
+        className="input h-9 w-48 sm:w-53.5"
+        value={value}
+        onChange={onChange}
+      />
+      {cross}
+      <button className="button px-2 py-1" onClick={onSearchClick}>
+        {search}
+      </button>
+    </div>
+  );
+}
